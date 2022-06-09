72f0d0351d1752819c292437107d599fcbec5d7d
nghiavt1302 <vtnghia01.bchkl@gmail.com>
2022-06-09 14:43:43 +0700

    Create project

---
+++ src/main/java/com/example/ghtk_student_assignment/GhtkStudentAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghtk_student_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhtkStudentAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhtkStudentAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ghtk_student_assignment/GhtkStudentAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghtk_student_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhtkStudentAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
